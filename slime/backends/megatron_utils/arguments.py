eec2c4bc935e96ae9e1574bfdfec969984f24149
MikaStars39 <meloxdmy@qq.com>
2026-04-20 20:05:10 +0800

    update

--- slime/backends/megatron_utils/arguments.py
+++ slime/backends/megatron_utils/arguments.py
@@ -103,7 +103,7 @@ def _hf_validate_args(args, hf_config):
 def _set_default_megatron_args(args):
     # Muon/Roo/SGD do not support distributed optimizer; for all other optimizers use ZeRO.
     opt = getattr(args, 'optimizer', 'adam')
-    if 'muon' in opt or opt in ('roo', 'sgd', 'muon_projected', 'gasd', 'soap'):
+    if 'muon' in opt or opt in ('roo', 'sgd', 'muon_projected', 'gasd', 'soap', 'rmsprop'):
         args.use_distributed_optimizer = False
     else:
         args.use_distributed_optimizer = True
